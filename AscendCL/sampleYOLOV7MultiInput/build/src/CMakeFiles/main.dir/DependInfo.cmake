e7ebaa5c03d970011f3670d1015ba5927adb187b
xmuhlw <771376679@qq.com>
2023-09-27 23:36:57 +0800

    更新视频推理

---
+++ AscendCL/sampleYOLOV7MultiInput/build/src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/AclLiteApp.cpp" "src/CMakeFiles/main.dir/__/common/src/AclLiteApp.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/AclLiteApp.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/AclLiteImageProc.cpp" "src/CMakeFiles/main.dir/__/common/src/AclLiteImageProc.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/AclLiteImageProc.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/AclLiteModel.cpp" "src/CMakeFiles/main.dir/__/common/src/AclLiteModel.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/AclLiteModel.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/AclLiteResource.cpp" "src/CMakeFiles/main.dir/__/common/src/AclLiteResource.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/AclLiteResource.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/AclLiteThread.cpp" "src/CMakeFiles/main.dir/__/common/src/AclLiteThread.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/AclLiteThread.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/AclLiteThreadMgr.cpp" "src/CMakeFiles/main.dir/__/common/src/AclLiteThreadMgr.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/AclLiteThreadMgr.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/AclLiteUtils.cpp" "src/CMakeFiles/main.dir/__/common/src/AclLiteUtils.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/AclLiteUtils.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/AclLiteVideoProc.cpp" "src/CMakeFiles/main.dir/__/common/src/AclLiteVideoProc.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/AclLiteVideoProc.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/CropAndPasteHelper.cpp" "src/CMakeFiles/main.dir/__/common/src/CropAndPasteHelper.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/CropAndPasteHelper.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/JpegDHelper.cpp" "src/CMakeFiles/main.dir/__/common/src/JpegDHelper.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/JpegDHelper.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/JpegEHelper.cpp" "src/CMakeFiles/main.dir/__/common/src/JpegEHelper.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/JpegEHelper.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/PngDHelper.cpp" "src/CMakeFiles/main.dir/__/common/src/PngDHelper.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/PngDHelper.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/ResizeHelper.cpp" "src/CMakeFiles/main.dir/__/common/src/ResizeHelper.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/ResizeHelper.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/VdecHelper.cpp" "src/CMakeFiles/main.dir/__/common/src/VdecHelper.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/VdecHelper.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/VencHelper.cpp" "src/CMakeFiles/main.dir/__/common/src/VencHelper.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/VencHelper.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/VideoCapture.cpp" "src/CMakeFiles/main.dir/__/common/src/VideoCapture.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/VideoCapture.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/common/src/VideoWriter.cpp" "src/CMakeFiles/main.dir/__/common/src/VideoWriter.cpp.o" "gcc" "src/CMakeFiles/main.dir/__/common/src/VideoWriter.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/src/dataInput/dataInput.cpp" "src/CMakeFiles/main.dir/dataInput/dataInput.cpp.o" "gcc" "src/CMakeFiles/main.dir/dataInput/dataInput.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/src/dataOutput/dataOutput.cpp" "src/CMakeFiles/main.dir/dataOutput/dataOutput.cpp.o" "gcc" "src/CMakeFiles/main.dir/dataOutput/dataOutput.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/src/detectInference/detectInference.cpp" "src/CMakeFiles/main.dir/detectInference/detectInference.cpp.o" "gcc" "src/CMakeFiles/main.dir/detectInference/detectInference.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/src/detectPostprocess/detectPostprocess.cpp" "src/CMakeFiles/main.dir/detectPostprocess/detectPostprocess.cpp.o" "gcc" "src/CMakeFiles/main.dir/detectPostprocess/detectPostprocess.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/src/detectPreprocess/detectPreprocess.cpp" "src/CMakeFiles/main.dir/detectPreprocess/detectPreprocess.cpp.o" "gcc" "src/CMakeFiles/main.dir/detectPreprocess/detectPreprocess.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/src/main.cpp" "src/CMakeFiles/main.dir/main.cpp.o" "gcc" "src/CMakeFiles/main.dir/main.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/src/pushrtsp/pictortsp.cpp" "src/CMakeFiles/main.dir/pushrtsp/pictortsp.cpp.o" "gcc" "src/CMakeFiles/main.dir/pushrtsp/pictortsp.cpp.o.d"
+  "/home/HwHiAiUser/sample/samples-master/inference/modelInference/sampleYOLOV7MultiInput/src/pushrtsp/pushrtspthread.cpp" "src/CMakeFiles/main.dir/pushrtsp/pushrtspthread.cpp.o" "gcc" "src/CMakeFiles/main.dir/pushrtsp/pushrtspthread.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
